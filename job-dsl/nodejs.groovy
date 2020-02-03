8acc29a97bbe67b7e8f3c442dea7ae031c2d671b
francisco <francisco.neto@daemoninvestimentos.com.br>
2020-02-03 10:49:55 -0300

    [FEAT] using job dsl for nodejs

---
+++ job-dsl/nodejs.groovy
@@ -0,0 +1,17 @@
+job('NodeJS example') {
+    scm (
+        git("git://github.com/fcomuniz/simple-node.git") { node -> 
+            node / gitConfigName("DSL User")
+            node / gitConfigEmail('jenkins-dsl@daemonquant.com')
+        }
+    )
+    triggers {
+        scm("H/5 * * * *")
+    }
+    wrappers {
+        nodejs("nodejs")
+    }
+    steps {
+        shell("npm install")
+    }
+}
