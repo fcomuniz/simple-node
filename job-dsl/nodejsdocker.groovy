3b89a6c3d1eecac10709086f9643600e80658fc1
francisco <francisco.neto@daemoninvestimentos.com.br>
2020-02-03 11:11:51 -0300

    [FEAT] nodejsdocker.groovy file for building and sending to docker

---
+++ job-dsl/nodejsdocker.groovy
@@ -0,0 +1,25 @@
+job('NodeJS Docker example'){
+    scm{
+        git('git://github.com/fcomuniz/simple-node.git') { node ->
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@daemonquant.com')
+        }
+        triggers {
+            scm('H/5 * * * *')
+        }
+        wrappers {
+            nodejs('nodejs')
+        }
+        steps {
+            dockerBuildAndPublish {
+                repositoryName("fcomuniz/sample-node")
+                tag("${GIT_REVISION},length=9")
+                registryCredentials("dockerhub")
+                forcePull(false)
+                forceTag(false)
+                createFingerprints(false)
+                skipDecorate()
+            }
+        }
+    }
+}
